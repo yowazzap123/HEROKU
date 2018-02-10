a0b480e96ab485ab65a878dfe3720e15e7d83321
yowazzap123 <36319733+yowazzap123@users.noreply.github.com>
2018-02-10 11:31:14 -0500

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("rest in spaghetti")) {
+  if (message.content.startsWith(rip)) {
     message.channel.send("NEVER FUCKING FORGETTI");
   }
   if (message.content.startsWith("you should eat pie")){
