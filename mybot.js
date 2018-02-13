4857160f01c12dfd84bdb023465d8ce34bbd0bd1
yowazzap123 <36319733+yowazzap123@users.noreply.github.com>
2018-02-12 22:57:32 -0500

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -20,7 +20,7 @@ client.on("message", (message) => {
     message.channel.send("Your rights:\n 1) Freedom of religion, speech, press, assembly, and petition.\n 2) Right to keep and bear arms in order to maintain a well regulated militia.\n 3) No quartering of soldiers.\n 4) Freedom from unreasonable searches and seizures.\n 5) Right to due process of law, freedom from self-incrimination, double jeopardy.\n 6) Rights of accused persons, e.g., right to a speedy and public trial.\n 7) Right of trial by jury in civil cases.\n 8) Freedom from excessive bail, cruel and unusual punishments.\n Other rights of the people.\n 10) Powers reserved to the states.\n 11)TO HAVE THE HUMAN RIGHT TO BE THE BIGGEST FUCK UPS");
   }
   if (input === "where are my rights?"){
-    message.channel.send("in the Bill of Rights faggot.)
+    message.channel.send("in the Bill of Rights faggot.")
   }
   if (input === "who is denz?"){
     message.channel.send("WHO IS DENZ??\n Denz is a talented swordsman\n Who has never let fear best him in a fight.\n Perhaps that stems from the burden he bears... The Templar's Curse. \n It is a terrible power that in the coming days, will shape his future...");
