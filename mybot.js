9df4d140410d6c88b8f177c008052eb9b1ba35f3
yowazzap123 <36319733+yowazzap123@users.noreply.github.com>
2018-02-12 22:05:04 -0500

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -25,7 +25,7 @@ client.on("message", (message) => {
       message.channel.send("10) Powers reserved to the states.")
   }
   if (message.content.startsWith("What are my rights?")){
-      message.channel.send("\n 1) Freedom of religion, speech, press, assembly, and petition.\n 2) Right to keep and bear arms in order to maintain a well regulated militia.")
+      message.channel.send("1) Freedom of religion, speech, press, assembly, and petition.\n 2) Right to keep and bear arms in order to maintain a well regulated militia.\n 3) No quartering of soldiers.\n 4) Freedom from unreasonable searches and seizures.\n 5) Right to due process of law, freedom from self-incrimination, double jeopardy.\n 6) Rights of accused persons, e.g., right to a speedy and public trial.\n 7) Right of trial by jury in civil cases.\n 8) Freedom from excessive bail, cruel and unusual punishments.\n Other rights of the people.\n 10) Powers reserved to the states.\n 11)TO HAVE THE HUMAN RIGHT TO BE THE BIGGEST FUCK UPS")
   
   }
 
